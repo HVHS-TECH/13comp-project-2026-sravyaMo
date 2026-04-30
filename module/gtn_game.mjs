203bdc27ce31f552d169035513a655d284e74e19
sravyaMo <22073sm@hvhs.school.nz>
2026-04-30 01:16:15 +0000

    GTN on code simulator works. now make for 2 players

--- module/gtn_game.mjs
+++ module/gtn_game.mjs
@@ -14,4 +14,32 @@ const resetGame = () => {
     guess.style.color = "#333"; // repeat guess color of text
     input.value = ""; // clear input field
     checkButton.textContent = "check"; //reset button text
-}
+};
+
+//generate number between 0 and 99
+let randomNum = Math.floor(Math.random() * 100);
+
+// add click event listener to check button
+checkButton.addEventListener("click", () => {
+    if (input.disabled) {
+        //if input disabled, reset game
+        resetGame ();
+        return;
+    }
+
+    let inputValue = input.value; //gets value from input page
+
+    if (inputValue == randomNum) { // correct guess
+        [guess.textContent, input.disabled] = ["Correct number guessed!", true];
+        [checkButton.textContent, guess.style.color] = ["Replay", "#27ae60"];
+    } else if (inputValue > randomNum && inputValue < 100) { //guess too high
+        [guess.textContent] = ["Guess too high"];
+        guess.style.color = "#333";
+    } else if (inputValue < randomNum && inputValue > 0) { //guess too low
+        [guess.textContent] = ["Guess too low"];
+        guess.style.color = "#333";
+    } else { // for invalid input not in range 1-99
+        [guess.textContent] = ["Invalid guess!"];
+        guess.style.color = "#e74c3c";
+    } 
+});
