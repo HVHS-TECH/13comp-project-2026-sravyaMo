91ca01a0f0c4192c3dce79b8181c39e93a9a5c12
sravyaMo <22073sm@hvhs.school.nz>
2026-05-11 21:57:09 +0000

    working on game

---
+++ module/gtn_lobby.js
@@ -0,0 +1,17 @@
+// Popup elements
+const popup = document.querySelector(".popup");
+const popupTitle = document.querySelector("#popupTitle");
+const popupMessage = document.querySelector(".popupMessage");
+
+// Buttons
+const createBtn = document.querySelector("#createBtn");
+const joinBtn = document.querySelector("#joinBtn");
+const submitBtn = document.querySelector("#submitBtn");
+const closeBtn = document.querySelector("#submitBtn");
+
+// Input 
+const serverInput = document.querySelector("#serverInput");
+
+// Current mode
+let mode = ""
+
