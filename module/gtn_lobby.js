61d713adfbfd232ff286b149e31a05b11d681f53
sravyaMo <22073sm@hvhs.school.nz>
2026-05-13 00:29:15 +0000

    working on lobby,

--- module/gtn_lobby.js
+++ module/gtn_lobby.js
@@ -15,3 +15,57 @@ const serverInput = document.querySelector("#serverInput");
 // Current mode
 let mode = ""
 
+// Opening the "Create" popup
+createBtn.addEventListener("click", () => {
+    mode = "create";
+    popup.classList.remove("hidden");
+    popupTitle.textContent = "Create Server";
+    popupMessage.textContent = "";
+    serverInput.value = "";
+});
+
+// Opening the "Join" popup
+joinBtn.addEventListener("click", () => {
+    mode = "join";
+    popup.classList.remove("hidden");
+    popupTitle.textContent = "Join Server";
+    popupMessage.textContent = "";
+    serverInput.value = "";
+});
+
+// Closing the popup
+closeBtn.addEventListener("click", () => {
+    popup.classList.add("hidden");
+});
+
+// Submitting button
+submitBtn.addEventListener("click", () => {
+    const roomName = serverInput.value.trim();
+
+    // Validation part
+    if (roomName < 3) {
+        popupMessage.textContent = "Minimum 3 letters";
+        return;
+    }
+
+    if (roomName > 12) {
+        popupMessage.textContent = "Maximum 12 letters";
+        return;
+    }
+
+    // Save room name
+    localStorage.setItem("roomName", roomName);
+
+    // Save mode
+    localStorage.setItem("roomMode", mode);
+
+    // Move to game
+    window.location.href = "/html/gtn_game.html";
+});
+
+// Enter key support
+serverInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        submitBtn.click();
+    }
+});
